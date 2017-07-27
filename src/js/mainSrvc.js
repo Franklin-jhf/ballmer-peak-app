dd6a756b2fc7b68fdf2ff27febcfecb08cde2527
Franklin Floresca <franklin.jhf@gmail.com>
2017-07-27 17:23:27 -0500

    about to start on api work

--- src/js/mainSrvc.js
+++ src/js/mainSrvc.js
@@ -1,8 +1,16 @@
-app.service('mainSrvc', [function () {
+app.service('mainSrvc', [function ($http) {
+
+	// CALCULATOR FUNCTION
 	this.calc = (alcAmt, alcConcentation, weight, gender, time) => {
-		var percent = alcAmt * alcConcentation * 5.14 / weight;
+		var percent = alcAmt * alcConcentation / 100 * 5.14 / weight;
 		if (gender === 'male') {percent *= 0.73};
 		if (gender === 'female') {percent *= 0.66};
-		return percent - 0.015 * time;
+		return (percent - 0.015 * time).toFixed(3);
+	}
+
+	// GOOGLE MAP HTTP REQ
+	this.map = () => {
+		return $http 
+		//
 	}
 }])
